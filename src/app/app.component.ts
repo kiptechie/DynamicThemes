64d20515b26e8534aa013002dd62b43cb15328fa
Timothy Serem <kipkorirserem3@gmail.com>
2019-07-17 10:46:22 +0300

    plugin update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { FcmService } from './fcm.service';
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,9 +15,31 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm: FcmService,
+    public toastController: ToastController
   ) {
     this.initializeApp();
+  }
+
+  private async presentToast(message) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  private notificationSetup() {
+    this.fcm.getToken();
+    this.fcm.onNotifications().subscribe(
+      (msg) => {
+        if (this.platform.is('ios')) {
+          this.presentToast(msg.aps.alert);
+        } else {
+          this.presentToast(msg.body);
+        }
+      });
   }
 
   initializeApp() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,20 @@ import { AppComponent } from './app.component';
 
 import { IonicStorageModule } from '@ionic/storage';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+const config = {
+  apiKey: 'AIzaSyCbtFnX3rKUGKSaIuWzNVKNYJRd-YNvveA',
+  authDomain: 'my-ionic-apps.firebaseapp.com',
+  databaseURL: 'https://my-ionic-apps.firebaseio.com',
+  projectId: 'my-ionic-apps',
+  storageBucket: 'my-ionic-apps.appspot.com',
+  messagingSenderId: '132692263367',
+  appId: '1:132692263367:web:9ebb2f930f65fdab'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -18,11 +32,14 @@ import { IonicStorageModule } from '@ionic/storage';
     BrowserModule,
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
     AppRoutingModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
